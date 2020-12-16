aec30b541ff40544ea5a876b38c2698bd9bc08ce
Diego Castillo <diegocasmo@gmail.com>
2020-12-16 13:50:48 -0500

    fix(Expo): use expo-app-loading package

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { AppLoading } from 'expo'
+import AppLoading from 'expo-app-loading'
 import * as Font from 'expo-font'
 import { Ionicons } from '@expo/vector-icons'
 
