baa9194e0bc91d718e3de4722cdc65b616780991
Diego Castillo <diegocasmo@gmail.com>
2022-04-30 08:35:25 -0500

    chore(routines): re-use 'useRoutines' hook in 'useRoutine'

--- src/services/routines/useRoutine.js
+++ src/services/routines/useRoutine.js
@@ -1,14 +1,7 @@
-import { useQuery } from 'react-query'
-
-import { makeApiService } from '_api/client'
-import { ROUTINES_QUERY_KEY } from '_services/routines/useRoutines'
+import { useRoutines } from '_services/routines/useRoutines'
 import { findRoutineById } from '_state/selectors/routine'
 
-const getRoutines = makeApiService({
-  path: 'routines',
-})
-
 export const useRoutine = (routineId) =>
-  useQuery(ROUTINES_QUERY_KEY, getRoutines, {
+  useRoutines({
     select: (data) => findRoutineById(data, routineId),
   })
--- src/services/routines/useRoutines.js
+++ src/services/routines/useRoutines.js
@@ -11,4 +11,5 @@ export const ROUTINES_QUERY_KEY = ['routines']
 export const invalidateRoutines = async (queryClient) =>
   queryClient.invalidateQueries(ROUTINES_QUERY_KEY)
 
-export const useRoutines = () => useQuery(ROUTINES_QUERY_KEY, getRoutines)
+export const useRoutines = (options = {}) =>
+  useQuery(ROUTINES_QUERY_KEY, getRoutines, options)
