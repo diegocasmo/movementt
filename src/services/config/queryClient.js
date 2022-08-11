c1787e565d568f85be1ca1a029ff2b079660d39c
Diego Castillo <diegocasmo@gmail.com>
2022-08-11 13:20:06 -0500

    Avoid retries on 401

--- src/services/config/queryClient.js
+++ src/services/config/queryClient.js
@@ -1,3 +1,23 @@
 import { QueryClient } from 'react-query'
 
-export const queryClient = new QueryClient()
+function createQueryRetryCondition(
+  acceptedErrorStatusCodes = [401],
+  maxRetries = 3
+) {
+  const acceptedStatusCodesSet = new Set(acceptedErrorStatusCodes)
+
+  return function shouldRetry(failureCount, error) {
+    return (
+      !acceptedStatusCodesSet.has(error.response?.status) &&
+      failureCount <= maxRetries
+    )
+  }
+}
+
+export const defaultOptions = {
+  queries: {
+    retry: createQueryRetryCondition(),
+  },
+}
+
+export const queryClient = new QueryClient({ defaultOptions })
